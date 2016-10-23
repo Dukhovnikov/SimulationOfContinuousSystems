306007aad96294cba913b9da38729535cc13af8f
Dukhovnikov <Dukhovnikov@DESKTOP-11DUQJQ>
2016-10-24 00:15:04 +0300

    Удалено ненужное.

--- frqvs/MainWindow.xaml.cs
+++ frqvs/MainWindow.xaml.cs
@@ -74,17 +74,14 @@ namespace frqvs
 
         private void ID_INTERNET_Click(object sender, RoutedEventArgs e)
         {
-            //IDD_INT INT = new IDD_INT();
-            //INT.ShowDialog();
-            IDC_WebBrowser WebBrowser = new IDC_WebBrowser();
-            WebBrowser.ShowDialog();
+
+            Data.Browser.ShowDialog();
         }
 
         private void ID_FILE_SAVE_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog myDialog = new SaveFileDialog();
             myDialog.Filter = "Таблица(*.csv)|*.csv;";
-            //myDialog.CheckFileExists = true;
             if (myDialog.ShowDialog() == true)
             {
                 string filename = myDialog.FileName;
@@ -92,6 +89,18 @@ namespace frqvs
                 file.WriteLine(Data.ConvertToStringForFile());
                 file.Close();
             }
+        }
+
+        private void ID_SYS_Click(object sender, RoutedEventArgs e)
+        {
+            IDC_WebBrowser WebBrowser = new IDC_WebBrowser();
+            Data.Browser = WebBrowser;
+        }
+
+        private void ID_PRIV_Click(object sender, RoutedEventArgs e)
+        {
+            IDD_INT INT = new IDD_INT();
+            Data.Browser = INT;
         }
     }
 }
